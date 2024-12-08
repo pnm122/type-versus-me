28020225b46594d2759c87674a3c7e3ec4c92829
pnm122 <pnmartin02@gmail.com>
2024-12-08 00:49:16 -0500

    Generate basic random puzzles

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 [x] store cursor positions differently so that the current user's typing doesn't move other cursors
 [ ] disallow copying/pasting
-[ ] generate random tests
+[x] generate random tests
 [x] calculate statistics for whole test
 [ ] calculate statistics in realtime as the user types
+[ ] fix text going offscreen in Typer
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,17 @@
 
 import Typer, { TyperStats } from '@/components/Typer/Typer'
 import styles from './style.module.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import generatePuzzle from '@/utils/generatePuzzle'
 
 export default function Home() {
   const [finished, setFinished] = useState(false)
   const [stats, setStats] = useState<TyperStats | null>(null)
-  const [position, setPosition] = useState(12)
+  const [test, setTest] = useState<string>('')
+
+  useEffect(() => {
+    setTest(generatePuzzle(100))
+  }, [])
 
   function onStart(t: number) {
   }
@@ -23,7 +28,7 @@ export default function Home() {
   return (
     <div className={styles['main']}>
       <Typer
-        text='So if on advanced addition absolute received replying throwing he. Delighted consisted newspaper of unfeeling as neglected so. Tell size come hard mrs and four fond are. Of in commanded earnestly resources it.'
+        text={test}
         finished={finished}
         onChange={onChange}
         onStart={onStart}
@@ -50,13 +55,6 @@ export default function Home() {
             letter: 6
           }
         }]}
-      />
-      <input
-        type='range'
-        min='0'
-        max='200'
-        value={position}
-        onChange={e => setPosition(parseInt(e.target.value))}
       />
       {stats && (
         <div style={{ marginTop: 24 }}>
