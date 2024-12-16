f9bef1919949248c41167324f31c9dd0bb396324
pnm122 <pnmartin02@gmail.com>
2024-12-15 23:10:07 -0500

    Add unplugin icons

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import UnpluginIcons from 'unplugin-icons/webpack'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.plugins.push(
+      UnpluginIcons({
+        compiler: 'jsx',
+        jsx: 'react'
+      })
+    )
+
+    return config
+  },
 };
 
 export default nextConfig;
