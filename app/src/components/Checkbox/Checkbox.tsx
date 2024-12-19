4f46b8fe1bf9466ae4973eb627d4ded005de4274
pnm122 <pnmartin02@gmail.com>
2024-12-19 17:03:35 -0500

    Add Checkbox component

---
+++ app/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren } from 'react'
+import styles from './style.module.scss'
+import PixelarticonsCheck from '~icons/pixelarticons/check'
+
+type Props = PropsWithChildren<{
+  checked: boolean
+  onChange?: (value: boolean) => void
+}>
+
+export default function Checkbox({
+  checked,
+  onChange = () => {},
+  children
+}: Props) {
+  return (
+    <button
+      className={styles['checkbox']}
+      role='checkbox'
+      aria-checked={checked}
+      onClick={() => onChange(!checked)}>
+      <div className={styles['checkbox__box']}>
+        <PixelarticonsCheck className={styles['check']} />
+      </div>
+      {children}
+    </button>
+  )
+}
