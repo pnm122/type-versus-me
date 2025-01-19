e1028ebb0884de70b7f4f4676f155b183618f560
pnm122 <pnmartin02@gmail.com>
2025-01-19 02:06:37 -0500

    Add simple error handling to getUserStats

--- app/src/utils/database/user.ts
+++ app/src/utils/database/user.ts
@@ -32,38 +32,46 @@ export async function getUserStats(
 		}
 	}
 
-	const { _avg, _max, _count } = await prisma.score.aggregate({
-		where,
-		_avg: {
-			netWPM: true
-		},
-		_max: {
-			netWPM: true
-		},
-		_count: {
-			raceId: true,
-			isWinner: true
-		}
-	})
-
-	const wordsTyped = (
-		await prisma.score.findMany({
+	try {
+		const { _avg, _max, _count } = await prisma.score.aggregate({
 			where,
-			select: {
-				race: {
-					select: {
-						numWords: true
-					}
-				}
+			_avg: {
+				netWPM: true
+			},
+			_max: {
+				netWPM: true
+			},
+			_count: {
+				raceId: true,
+				isWinner: true
 			}
 		})
-	).reduce((sum, curr) => sum + curr.race.numWords, 0)
 
-	return {
-		wordsTyped,
-		maxWPM: _max.netWPM ?? -1,
-		avgWPM: _avg.netWPM ?? -1,
-		racesPlayed: _count.raceId ?? -1,
-		racesWon: _count.isWinner ?? -1
+		const wordsTyped = (
+			await prisma.score.findMany({
+				where,
+				select: {
+					race: {
+						select: {
+							numWords: true
+						}
+					}
+				}
+			})
+		).reduce((sum, curr) => sum + curr.race.numWords, 0)
+
+		return {
+			wordsTyped,
+			maxWPM: _max.netWPM ?? -1,
+			avgWPM: _avg.netWPM ?? -1,
+			racesPlayed: _count.raceId ?? -1,
+			racesWon: _count.isWinner ?? -1
+		}
+	} catch (e) {
+		if (e instanceof Prisma.PrismaClientKnownRequestError) {
+			console.error(`${e.code}:`, e.message)
+		}
+		console.error(e)
+		return null
 	}
 }
