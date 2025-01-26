126c88658cf0741384f856d082d398896f63760a
pnm122 <pnmartin02@gmail.com>
2025-01-26 09:17:21 -0500

    Subscribe to current user changes if the profile is for the current user

--- app/src/components/page/profile/UserInfo/UserInfo.tsx
+++ app/src/components/page/profile/UserInfo/UserInfo.tsx
@@ -1,24 +1,31 @@
+'use client'
+
 import { User } from '@prisma/client'
 import styles from './style.module.scss'
 import UserAndCursor from '@/components/shared/UserAndCursor/UserAndCursor'
 import { CursorColor } from '$shared/types/Cursor'
 import LevelAndPoints from '../LevelAndPoints/LevelAndPoints'
+import { useAuthContext } from '@/context/Auth'
 
 interface Props {
 	user: User
 }
 
 export default function UserInfo({ user }: Props) {
+	const { user: currentUser } = useAuthContext()
+	// Subscribe to current user changes if this is the current user
+	const displayUser = user.id === currentUser?.id ? currentUser : user
+
 	return (
 		<div className={styles['user']}>
 			<div className={styles['user__preview-and-edit']}>
 				<UserAndCursor
 					size="large"
-					username={user.username}
-					color={user.cursorColor as CursorColor}
+					username={displayUser.username}
+					color={displayUser.cursorColor as CursorColor}
 				/>
 			</div>
-			<LevelAndPoints user={user} />
+			<LevelAndPoints user={displayUser} />
 		</div>
 	)
 }
