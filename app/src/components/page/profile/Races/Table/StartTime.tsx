edd23249b9688d48159bff586bf7e661375f38c6
pnm122 <pnmartin02@gmail.com>
2025-01-27 00:14:16 -0500

    Fix start time text having plurals when the number is 1

--- app/src/components/page/profile/Races/Table/StartTime.tsx
+++ app/src/components/page/profile/Races/Table/StartTime.tsx
@@ -1,22 +1,28 @@
 import styles from './style.module.scss'
 
 export default function StartTime({ startTime }: { startTime: Date }) {
-	const currentTime = new Date()
-	const difference = currentTime.getTime() - startTime.getTime()
 	const MS_PER_SECOND = 1000
 	const MS_PER_MINUTE = MS_PER_SECOND * 60
 	const MS_PER_HOUR = MS_PER_MINUTE * 60
 	const MS_PER_DAY = MS_PER_HOUR * 24
 
+	const currentTime = new Date()
+	const difference = currentTime.getTime() - startTime.getTime()
+
+	const secondsAgo = Math.round(difference / MS_PER_SECOND)
+	const minutesAgo = Math.round(difference / MS_PER_MINUTE)
+	const hoursAgo = Math.round(difference / MS_PER_HOUR)
+	const daysAgo = Math.round(difference / MS_PER_DAY)
+
 	const displayText =
-		difference < MS_PER_MINUTE
-			? `${Math.round(difference / MS_PER_SECOND)} seconds ago`
+		secondsAgo < 60
+			? `${secondsAgo} second${secondsAgo > 1 ? 's' : ''} ago`
 			: difference < MS_PER_HOUR
-				? `${Math.round(difference / MS_PER_MINUTE)} minutes ago`
+				? `${minutesAgo} minute${minutesAgo > 1 ? 's' : ''} ago`
 				: difference < MS_PER_DAY
-					? `${Math.round(difference / MS_PER_HOUR)} hours ago`
+					? `${hoursAgo} hour${hoursAgo > 1 ? 's' : ''} ago`
 					: difference < 7 * MS_PER_DAY
-						? `${Math.round(difference / MS_PER_DAY)} days ago`
+						? `${daysAgo} day${daysAgo > 1 ? 's' : ''} ago`
 						: Intl.DateTimeFormat().format(startTime)
 
 	return <span className={styles['time']}>{displayText}</span>
