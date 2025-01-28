7d12f26173f290e007ae6a6cb8b1e64c6b0dfac1
pnm122 <pnmartin02@gmail.com>
2025-01-27 20:16:57 -0500

    Prevent unnecessary rerenders of profile stats

--- app/src/components/page/profile/Stats/StatsContentClient.tsx
+++ app/src/components/page/profile/Stats/StatsContentClient.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useTransition } from 'react'
+import { memo, useTransition } from 'react'
 import CategoryFilter from '@/components/page/profile/Filter/CategoryFilter'
 import styles from './style.module.scss'
 import createClasses from '@/utils/createClasses'
@@ -16,54 +16,64 @@ import {
 import formatNumber from '@/utils/formatNumber'
 import CountUp from 'react-countup'
 
-export default function StatsContentClient({ stats }: { stats: UserStats | null }) {
-	const [isPending, startTransition] = useTransition()
+const StatsContentClient = memo(
+	({ stats }: { stats: UserStats | null }) => {
+		const [isPending, startTransition] = useTransition()
 
-	return (
-		<div className={styles['stats']}>
-			<h2 className={styles['stats__heading']}>Stats</h2>
-			<div className={styles['stats__filters']}>
-				<CategoryFilter paramKey={CATEGORY_PARAM_KEY} transition={[isPending, startTransition]} />
-				<NumWordsFilter
-					minWordsParamKey={MIN_WORDS_PARAM_KEY}
-					maxWordsParamKey={MAX_WORDS_PARAM_KEY}
-					transition={[isPending, startTransition]}
-				/>
+		return (
+			<div className={styles['stats']}>
+				<h2 className={styles['stats__heading']}>Stats</h2>
+				<div className={styles['stats__filters']}>
+					<CategoryFilter paramKey={CATEGORY_PARAM_KEY} transition={[isPending, startTransition]} />
+					<NumWordsFilter
+						minWordsParamKey={MIN_WORDS_PARAM_KEY}
+						maxWordsParamKey={MAX_WORDS_PARAM_KEY}
+						transition={[isPending, startTransition]}
+					/>
+				</div>
+				<div className={styles['stat-boxes']}>
+					{Object.keys(userStatsDisplayNames).map((key) => (
+						<div
+							key={key}
+							className={createClasses({
+								[styles['stat-box']]: true,
+								[styles['stat-box--pending']]: isPending
+							})}
+						>
+							{isPending && <IndeterminateProgress />}
+							<h3 className={styles['stat-box__stat']}>
+								{stats === null ? (
+									'-'
+								) : stats[key as keyof typeof stats] < 0 ? (
+									0
+								) : (
+									<CountUp
+										start={0}
+										end={Math.round(stats[key as keyof typeof stats])}
+										duration={1}
+										formattingFn={(n) =>
+											`${formatNumber(n, true)}${['maxWPM', 'avgWPM'].includes(key) ? 'wpm' : ''}`
+										}
+									/>
+								)}
+							</h3>
+							<h4 className={styles['stat-box__name']}>
+								{userStatsDisplayNames[key as keyof typeof userStatsDisplayNames]}
+							</h4>
+						</div>
+					))}
+				</div>
 			</div>
-			<div className={styles['stat-boxes']}>
-				{Object.keys(userStatsDisplayNames).map((key) => (
-					<div
-						key={key}
-						className={createClasses({
-							[styles['stat-box']]: true,
-							[styles['stat-box--pending']]: isPending
-						})}
-					>
-						{isPending && <IndeterminateProgress />}
-						<h3 className={styles['stat-box__stat']}>
-							{stats === null ? (
-								'-'
-							) : stats[key as keyof typeof stats] < 0 ? (
-								0
-							) : (
-								<CountUp
-									start={0}
-									end={Math.round(stats[key as keyof typeof stats])}
-									duration={1}
-									formattingFn={(n) =>
-										`${formatNumber(n, true)}${['maxWPM', 'avgWPM'].includes(key) ? 'wpm' : ''}`
-									}
-									// Force all items to recount when any stat changes
-									key={stats.toString()}
-								/>
-							)}
-						</h3>
-						<h4 className={styles['stat-box__name']}>
-							{userStatsDisplayNames[key as keyof typeof userStatsDisplayNames]}
-						</h4>
-					</div>
-				))}
-			</div>
-		</div>
-	)
-}
+		)
+	},
+	(prev, next) =>
+		prev.stats?.avgWPM === next.stats?.avgWPM &&
+		prev.stats?.maxWPM === next.stats?.maxWPM &&
+		prev.stats?.racesPlayed === next.stats?.racesPlayed &&
+		prev.stats?.racesWon === next.stats?.racesWon &&
+		prev.stats?.wordsTyped === next.stats?.wordsTyped
+)
+
+StatsContentClient.displayName = 'StatsContentClient'
+
+export default StatsContentClient
