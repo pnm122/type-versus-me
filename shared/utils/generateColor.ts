b28984831eeaa654cc727352765ba83825d00c99
pnm122 <pnmartin02@gmail.com>
2025-01-30 15:57:50 -0500

    Split incorrect underline style

--- shared/utils/generateColor.ts
+++
@@ -1,8 +0,0 @@
-import { CursorColor } from '../types/Cursor'
-import CursorColors from './CursorColors'
-
-export function generateColor(
-	colors: Readonly<CursorColor[]> | CursorColor[] = CursorColors
-): CursorColor {
-	return colors[Math.round(Math.random() * (colors.length - 1))]!
-}
