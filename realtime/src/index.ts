64fea0d6dcbea25b5e5bd7fd6e498b4e0bac7f05
pnm122 <pnmartin02@gmail.com>
2025-01-30 15:28:10 -0500

    Remove register event

--- realtime/src/index.ts
+++ realtime/src/index.ts
@@ -1,5 +1,4 @@
 import io from './global/server'
-import Register from './events/Register'
 import CreateRoom from './events/CreateRoom'
 import JoinRoom from './events/JoinRoom'
 import ChangeUsername from './events/ChangeUsername'
@@ -23,7 +22,6 @@ io.on('connect', (socket) => {
 		io.emit('change-user-count', io.engine.clientsCount)
 	}, 250)
 
-	socket.on('register', (...args) => Register(socket, ...args))
 	socket.on('create-room', (...args) => CreateRoom(socket, ...args))
 	socket.on('join-room', (...args) => JoinRoom(socket, ...args))
 	socket.on('change-username', (...args) => ChangeUsername(socket, ...args))
--- shared/types/events/client/Register.ts
+++
@@ -1,8 +0,0 @@
-import { Return } from '../../Return'
-import { User } from '../../User'
-
-type Payload = Partial<Pick<User, 'username' | 'color'>>
-
-type Callback = (value: Return<User, 'invalid-username' | 'invalid-color'>) => void
-
-export type { Payload as RegisterPayload, Callback as RegisterCallback }
