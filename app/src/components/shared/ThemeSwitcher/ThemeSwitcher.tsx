435a1df391ce6acef1da02761780bbc7079770a6
pnm122 <pnmartin02@gmail.com>
2025-01-31 12:58:55 -0500

    Add system theme to ThemeSwitcher

--- app/src/components/shared/ThemeSwitcher/ThemeSwitcher.tsx
+++ app/src/components/shared/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,60 +3,88 @@
 import styles from './style.module.scss'
 import PixelarticonsSunAlt from '~icons/pixelarticons/sun-alt'
 import PixelarticonsMoon from '~icons/pixelarticons/moon'
-import createClasses from '@/utils/createClasses'
-import { useEffect, useRef, useState } from 'react'
+import PixelarticonsDeviceLaptop from '~icons/pixelarticons/device-laptop'
+import { useEffect, useId, useRef, useState } from 'react'
 import { useTheme } from 'next-themes'
 import SelectedBox from '@/components/base/SelectedBox/SelectedBox'
 
 export default function ThemeSwitcher({
 	ref
 }: {
-	ref?: React.RefObject<HTMLButtonElement | null>
+	ref?: React.RefObject<HTMLFieldSetElement | null>
 }) {
 	const { theme, setTheme } = useTheme()
-	const lightTheme = useRef<HTMLDivElement>(null)
-	const darkTheme = useRef<HTMLDivElement>(null)
+	const id = useId()
+	const lightRef = useRef<HTMLLabelElement>(null)
+	const darkRef = useRef<HTMLLabelElement>(null)
+	const systemRef = useRef<HTMLLabelElement>(null)
 	// Set to true after the initial render
 	// Suppresses hydration warning caused by next-themes
 	const [mounted, setMounted] = useState(false)
 
-	const isLightTheme = theme === 'light'
+	const selectedRef = theme === 'system' ? systemRef : theme === 'light' ? lightRef : darkRef
 
 	useEffect(() => {
 		setMounted(true)
 	}, [])
 
 	return (
-		<button
-			ref={ref}
-			role="checkbox"
-			// Only set properties relying on the theme state after mounting to avoid hydration errors
-			aria-checked={mounted && isLightTheme}
-			aria-label="Use light mode"
-			className={styles['switcher']}
-			onClick={() => setTheme(isLightTheme ? 'dark' : 'light')}
-		>
-			<SelectedBox selected={mounted ? (isLightTheme ? lightTheme : darkTheme) : undefined} />
-			<div
-				ref={darkTheme}
-				className={createClasses({
-					[styles['theme']]: true,
-					[styles['theme--selected']]: mounted && !isLightTheme
-				})}
-			>
-				<PixelarticonsMoon className={styles['theme__icon']} />
-				<p className={styles['theme__text']}>Dark</p>
+		<fieldset ref={ref} className={styles['switcher']} aria-label="Switch themes">
+			<div className={styles['themes']}>
+				<SelectedBox
+					className={styles['themes__selected-box']}
+					selected={mounted ? selectedRef : undefined}
+				/>
+				<label
+					ref={systemRef}
+					className={styles['theme']}
+					htmlFor={`theme-system-${id}`}
+					aria-label="System"
+				>
+					<PixelarticonsDeviceLaptop className={styles['theme__icon']} />
+					<input
+						name={`theme-switcher-${id}`}
+						id={`theme-system-${id}`}
+						type="radio"
+						checked={theme === 'system'}
+						onChange={() => setTheme('system')}
+						className={styles['theme__radio']}
+					/>
+				</label>
+				<label
+					ref={darkRef}
+					className={styles['theme']}
+					htmlFor={`theme-dark-${id}`}
+					aria-label="Dark"
+				>
+					<PixelarticonsMoon className={styles['theme__icon']} />
+					<input
+						name={`theme-switcher-${id}`}
+						id={`theme-dark-${id}`}
+						type="radio"
+						checked={theme === 'dark'}
+						onChange={() => setTheme('dark')}
+						className={styles['theme__radio']}
+					/>
+				</label>
+				<label
+					ref={lightRef}
+					className={styles['theme']}
+					htmlFor={`theme-light-${id}`}
+					aria-label="Light"
+				>
+					<PixelarticonsSunAlt className={styles['theme__icon']} />
+					{/* <span className={styles['theme__text']}>Light</span> */}
+					<input
+						name={`theme-switcher-${id}`}
+						id={`theme-light-${id}`}
+						type="radio"
+						checked={theme === 'light'}
+						onChange={() => setTheme('light')}
+						className={styles['theme__radio']}
+					/>
+				</label>
 			</div>
-			<div
-				ref={lightTheme}
-				className={createClasses({
-					[styles['theme']]: true,
-					[styles['theme--selected']]: mounted && isLightTheme
-				})}
-			>
-				<PixelarticonsSunAlt className={styles['theme__icon']} />
-				<p className={styles['theme__text']}>Light</p>
-			</div>
-		</button>
+		</fieldset>
 	)
 }
