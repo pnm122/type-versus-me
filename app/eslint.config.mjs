a81733d7a25354650daad8a3c0ae29695f4d0368
pnm122 <pnmartin02@gmail.com>
2026-07-08 03:56:56 -0400

    formatting

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
+import { defineConfig } from 'eslint/config'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
 
-export default defineConfig([{
-    extends: [...nextCoreWebVitals, ...nextTypescript],
-    rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "no-console": ["error", {
-            allow: ["error"],
-        }],
-        "react-hooks/exhaustive-deps": "off",
-        "react-hooks/set-state-in-effect": "warn",
-    },
-    settings: {
-      react: { version: "19" } // Avoids auto-detection crash (see https://github.com/vercel/next.js/issues/89764)
-    }
-}]);
+export default defineConfig([
+	{
+		extends: [...nextCoreWebVitals, ...nextTypescript],
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'no-console': [
+				'error',
+				{
+					allow: ['error']
+				}
+			],
+			'react-hooks/exhaustive-deps': 'off',
+			'react-hooks/set-state-in-effect': 'warn'
+		},
+		settings: {
+			react: { version: '19' } // Avoids auto-detection crash (see https://github.com/vercel/next.js/issues/89764)
+		}
+	}
+])
